eff2ed788d008121e9b5b41821c6726b1c5d04e2
Mateusz <mateuszhotowy@gmail.com>
2017-12-02 14:48:44 +0100

    Done.

---
+++ src/KotBuilder.java
@@ -0,0 +1,13 @@
+public class KotBuilder {
+
+
+    String integerNumbersToKot(int number75, int number79, int number84){
+
+
+        String result =
+                String.valueOf((char)((byte)number75))+
+                String.valueOf((char)((byte)number79))+
+                String.valueOf((char)((byte)number84));
+        return result;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Test {
+
+
+
+    public static void main(String[] args) throws AWTException {
+
+        KotBuilder kb = new KotBuilder();
+        System.out.println( kb.integerNumbersToKot(75,79,84));
+
+    }
+}
+
+
